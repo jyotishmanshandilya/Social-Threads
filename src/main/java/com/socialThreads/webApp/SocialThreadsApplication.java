a1c81bfbf3db99c600100cb551a1d87bc1ec0816
outcomes-jyotishman-shandilya <jshandilya@outcomes.com>
2025-09-13 17:27:36 +0530

    added all the base entities

--- src/main/java/com/socialThreads/webApp/SocialThreadsApplication.java
+++ src/main/java/com/socialThreads/webApp/SocialThreadsApplication.java
@@ -2,8 +2,12 @@ package com.socialThreads.webApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
+@EnableJdbcRepositories(basePackages = "com.socialThreads.webApp.repository")
+@EntityScan(basePackages = "com.socialThreads.webApp.model")
 public class SocialThreadsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/socialThreads/webApp/model/CommentLikes.java
@@ -0,0 +1,29 @@
+package com.socialThreads.webApp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentLikes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID commentLikeId;
+
+    private UUID commentId;
+    private UUID postId;
+    private UUID userId;
+    private LocalDateTime createdAt;
+}
